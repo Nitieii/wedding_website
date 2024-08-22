9e0e9001b3a6e2567d7a23a641f8093dc2cf1789
Ngo Tuan Anh <54606981+Nitieii@users.noreply.github.com>
2024-08-22 02:48:38 +0000

    chore: Add loader to landing page

--- sw.js
+++ sw.js
@@ -1,27 +1,4 @@
-// var cacheName = 'sw-v1'
-
-// const offlineUrl = 'offline/index.html'
-
-// this.addEventListener('install', e => {
-//     e.waitUntil(
-//         caches.open(cacheName).then(cache => cache.addAll([
-//             // '/media/offline/logo.svg',
-//             offlineUrl
-//         ]))
-//     )
-// })
-
-// this.addEventListener('fetch', e => {
-//     if (e.request.mode === 'navigate' || (e.request.method === 'GET' && e.request.headers.get('accept').includes('text/html'))) {
-//         e.respondWith(fetch(e.request.url).catch(error => {
-//             return caches.match(offlineUrl)
-//         }))
-//     }
-//     return
-// })
-
 $(document).ready(function () {
-
     var s_round = '.s_round';
 
     $(s_round).hover(function () {
